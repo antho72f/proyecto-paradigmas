212101e0ff4f5eab72adbce6758a26ab4cb544df
Thony <105994369+antho72f@users.noreply.github.com>
2024-03-07 23:56:56 -0600

    Generar imágenes mediante palabra clave "Necesito generar una imagen" "
    - Se implementa el consumo del api para generar imágenes
    - Se personaliza el prompt para que sea mas creativo
    - Se agrega el botón de nuevo chat y se ajusta el espaciado del componente estático.

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import OpenAI from "openai";
 
-const apiKey = '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';
+const apiKey = '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';
 const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true });
 
 export function Home() {
@@ -24,16 +24,33 @@ export function Home() {
     setInputValue("");
 
     try {
-      const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: inputValue }],
-      });
-    
-      const botMessage = response.choices[0].message.content.trim();
-      setMessages([...newMessages, { text: botMessage, isUser: false }]);
+      if (inputValue.toLowerCase().includes("generar una imagen")) {
+        const imageResponse = await openai.images.generate({ 
+          model: "dall-e-2", 
+          prompt: `Utiliza tu imaginación y crea una imagen sorprendente basada en "${inputValue}". ¡Sé tan creativo como puedas!` 
+        });
+        const imageURL = imageResponse.data[0].url;
+        setMessages([...newMessages, { text: `Aquí está la imagen generada:`, imageUrl: imageURL, isUser: false }]);
+      } else {
+        const response = await openai.chat.completions.create({
+          model: 'gpt-3.5-turbo',
+          messages: [{ 
+            role: 'user', 
+            content: `Déjate llevar por la creatividad y responde de manera original a "${inputValue}".` 
+          }],
+        });
+      
+        const botMessage = response.choices[0].message.content.trim();
+        setMessages([...newMessages, { text: botMessage, isUser: false }]);
+      }
     } catch (error) {
       console.error("Error al obtener la respuesta de OpenAI:", error);
     }    
+  };
+
+  const handleNewChat = () => {
+    localStorage.removeItem("messages");
+    setMessages([{ text: "Hola, ¿En qué te puedo ayudar hoy?", isUser: false }]);
   };
 
   return (
@@ -46,35 +63,59 @@ export function Home() {
           >
             <div className={`inline-block p-2 rounded-lg ${message.isUser ? "bg-blue-500 text-white" : "bg-gray-200 text-black"}`}>
               {message.text}
+              {message.imageUrl && (
+                <div className="image-container">
+                  <img src={message.imageUrl} alt="Generated Image" />
+                </div>
+              )}
             </div>
           </div>
         ))}
       </div>
-      <form onSubmit={sendMessage} className="bottom-fixed flex items-center justify-between px-4 py-2 border-t border-gray-300">
-        <input
-          type="text"
-          placeholder="Mensaje..."
-          className="w-2/3 md:w-2/3 sm:w-2/3 px-2 py-1 mr-2 border border-gray-300 rounded-md text-sm md:text-base focus:outline-none focus:border-blue-500"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button type="submit" className="w-1/3 md:w-1/3 sm:w-1/3 px-2 py-1 bg-blue-500 text-xs md:text-sm text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
-          Enviar
-        </button>
-      </form>
-
+      <div className="bottom-fixed flex items-center justify-between px-4 py-2 border-t border-gray-300 w-full max-w-screen-lg mx-auto">
+        <div className="flex-grow flex items-center">
+          <button
+            className="px-2 py-1 bg-gray-300 text-xs md:text-sm text-black rounded-md hover:bg-gray-400 focus:outline-none focus:bg-gray-400 mr-2"
+            onClick={handleNewChat}
+          >
+            Nuevo Chat
+          </button>
+          <input
+            type="text"
+            placeholder="Mensaje..."
+            className="flex-grow px-2 py-1 border border-gray-300 rounded-md text-sm md:text-base focus:outline-none focus:border-blue-500"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="px-2 py-1 bg-blue-500 text-xs md:text-sm text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 ml-2"
+            onClick={sendMessage}
+          >
+            Enviar
+          </button>
+        </div>
+      </div>
       <style>
         {`
         .bottom-fixed {
           position: fixed;
           bottom: 0;
-          width: 75%;
+          width: 100%;
+        }
+        .image-container {
+          max-width: 100%;
+          overflow: hidden;
+        }
+        .image-container img {
+          width: 40%;
+          height: auto;
+          display: block;
         }
         `}
       </style>
     </div>
   );
-  
 }
 
 export default Home;
