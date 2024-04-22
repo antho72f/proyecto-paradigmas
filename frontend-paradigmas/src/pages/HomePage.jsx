4ff6dde75bd8c87828fb314af0a980342298d009
Thony <105994369+antho72f@users.noreply.github.com>
2024-04-22 13:19:24 -0600

    Cambios mínimos

--- frontend-paradigmas/src/pages/HomePage.jsx
+++ frontend-paradigmas/src/pages/HomePage.jsx
@@ -2,26 +2,30 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-  <section className="bg-red-500 flex justify-center items-center">
-    <header className="bg-zinc-800 p-10">
-      <h1 className="text-5xl py-2 font-bold">React Tasks</h1>
-      <p className="text-md text-slate-400">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
-        fugit doloremque molestias recusandae labore repellat amet dicta tempore
-        necessitatibus facilis repellendus voluptas ducimus maiores deserunt sed
-        quo ratione provident debitis aut, voluptatem aliquam iste blanditiis
-        ex? Voluptatibus, fuga quasi necessitatibus cumque optio error enim,
-        officia accusantium vitae doloremque, molestias modi.
-      </p>
+    <section className="bg-red-500 flex justify-center items-center">
+      <header className="bg-zinc-800 p-10">
+        <h1 className="text-5xl py-2 font-bold">CreativeGPT</h1>
+        <p className="text-md text-slate-400">
+          El proyecto consiste en
+          desarrollar un software demostrativo que utilice
+          técnicas de generación automática de
+          contenido creativo en el lenguaje de
+          programación JavaScript. El objetivo principal
+          es explorar cómo los paradigmas de
+          programación pueden ser aplicados para
+          generar textos, imágenes u otros tipos de
+          contenido de forma automatizada y creativa.
 
-      <Link
-        className="bg-zinc-500 text-white px-4 py-2 rounded-md mt-4 inline-block"
-        to="/register"
-      >
-        Get Started
-      </Link>
-    </header>
-  </section>
+        </p>
+
+        <Link
+          className="bg-zinc-500 text-white px-4 py-2 rounded-md mt-4 inline-block"
+          to="/register"
+        >
+          Iniciar
+        </Link>
+      </header>
+    </section>
   );
 }
 
